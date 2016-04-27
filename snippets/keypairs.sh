be307d251e0bdea077fdde14ae39380066bdf004
dtrckd <ddtracked@gmail.com>
2016-04-27 17:20:25 +0200

    snippet openssh

---
+++ snippets/keypairs.sh
@@ -0,0 +1,15 @@
+# https://en.wikibooks.org/wiki/OpenSSH/Client_Configuration_Files
+
+### Create private and public keys (default -t rsa ???):
+ssh-keygen
+#.... The name-of-the-file
+#.... No password for passwordless login
+
+### Populate public key in a server
+ssh-copy-id -i <name-of-the-file> user@hostname
+
+### To add the add the public key in hosts_knows for authentification (avoid warning and error)
+echo  "hostname,ip-addr  $(cat <name-of-the-file.pub>)" >> ~/.ssh/known_hosts
+
+### Encrypt the new entry If ~/.ssh/known_hosts is encrypted (HashKnownHosts=True) :
+ssh-keygen -H
