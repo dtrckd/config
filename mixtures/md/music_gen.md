c1d5d1ed23db860492104656460b71ff7bebbb39
dtrckd <ddtracked@gmail.com>
2016-06-21 01:59:26 +0200

    gitignore + ssh

--- mixtures/md/music_gen.md
+++
@@ -1,5 +0,0 @@
-@music
-@recipe
-
-# SoundHelix
-http://txt.arboreus.com/2013/01/29/how-to-generate-some-music-with-soundhelix-and-save-it.html
--- mixtures/menu.php
+++
@@ -1,8 +0,0 @@
-
-  <ul> <h1>Outline</h1>
-      <ul>
-        <li> <a href="./index.php?rev=1&q=tags_all">All Tags</a></li> 
-      </ul>
-    <h1>TAG</h1>
-        <?php readfile("html/tags_list.html"); ?> 
-  </ul>
