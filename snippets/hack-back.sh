18708a58580e466df39551a71cb925d08f617c05
dtrckd <ddtracked@gmail.com>
2016-09-11 20:51:15 +0200

    giti back

---
+++ snippets/hack-back.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+DDD='home/joker'
+
+# Opera
+OperaBack="opera6.adr operaprefs.ini bookmarks.adr notes.adr search.ini speeddial.ini wand.dat\
+    keyboard menu styles toolbar skin sessions"
+
+for f in $FBACK; do
+    cp -rv "$f" ${DDD}/.opera/
+done
+
+# Icedove
+IcedoveBack="Cache ImapMail Mail OfflineCache content-perfs.sqlite directoryTree.json folderTree.json prefs.js"
+
+for f in $IcedoveBack; do
+    cp -rv "$f" ${DDD}/.icedove/
+done
+
+# Mc
+McBack="ini bindings menu"
+
+for f in $McBack; do
+    cp -rv "$f" ${DDD}/.config/.mc/
+done
+
