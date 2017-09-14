ec5789a4303ef8b70a5c276cda35617ac5f49bfe
dtrckd <ddtracked@gmail.com>
2017-09-14 13:31:11 +0200

    configure

--- configure/configure.sh
+++ configure/configure.sh
@@ -1,18 +1,36 @@
 #!/bin/bash
 
-#./install_from_scratch
+#
+# Init
+#
+rmdir --ignore-fail-on-non-empty ~/Public/
+mkdir -p ~/Music ~/Documents ~/Videos ~/SC ~/Desktop ~/src ~/bin
 
+######################
+### System
+######################
+apt-get install sudo aptitude apt-file apt-show-versions strace vim git 
 
 ######################
 ### Utils
 ######################
+aptitude install elinks mc rsync byobu wireshark ranger
 ranger --copy-config=all
 
+######################
+### Dev (python, c, fortran etc)
+######################
+# todo
+
+######################
+### Music (xmms2 plugin)
+######################
+aptitude install  xmms2 vlc audacity
 
 ######################
 ### Bluetooth
 ######################
-apt-get install pulseaudio-module-bluetooth blueman
+aptitude install pulseaudio-module-bluetooth blueman
 pactl load-module module-bluetooth-discover
 
 
@@ -21,9 +39,12 @@ pactl load-module module-bluetooth-discover
 ######################
 # @debug make webmain links...
 echo "Installing nginx..."
+# Kill apache ?
 aptitude install nginx 
 aptitude install php-fpm php php-cgi uwsgi
 echo "need to set up con files for sites..."
 P="/home/dtrckd/Desktop/workInProgress/conf/etc/nginx/sites-available/"
 cp $P/* /etc/nginx/sites-enabled/
 service nginx restart
+
+#./install_from_scratch
