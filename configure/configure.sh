68125b70ff452665883ae301c48a3d189fb4b08a
dtrckd <ddtracked@gmail.com>
2017-09-16 02:11:52 +0200

    q

--- configure/configure.sh
+++ configure/configure.sh
@@ -5,11 +5,12 @@
 #
 rmdir --ignore-fail-on-non-empty ~/Public/
 mkdir -p ~/Music ~/Documents ~/Videos ~/SC ~/Desktop ~/src ~/bin
+cp blue/* ~
 
 ######################
 ### System
 ######################
-apt-get install sudo aptitude python3-pip make ctags apt-file apt-show-versions htop strace vim git 
+apt-get install sudo aptitude python3-pip make ctags apt-file apt-show-versions htop strace vim git gitk gitg
 
 ######################
 ### Utils
@@ -20,8 +21,8 @@ ranger --copy-config=all
 ######################
 ### Dev (python, c, fortran etc)
 ######################
-# aptitude install python3-dev ?
-pip3 install --user ipython jupyter jupyter pyside matplotlib numpy scipy
+#aptitude install python3-dev gfortran libopenblas-dev # version ?
+pip3 install --user ipython jupyter matplotlib numpy scipy
 
 ######################
 ### Apps
@@ -31,7 +32,8 @@ aptitude install firefox thunderbird midori gimp libreoffice
 ######################
 ### Music (xmms2 plugin)
 ######################
-aptitude install xmms2 vlc audacity gxmm2
+aptitude install xmms2 vlc audacity xmms2 gxmm2 \
+    xmms2-plugin-alsa xmms2-plugin-asf xmms2-plugin-avcodec xmms2-plugin-faad xmms2-plugin-flac xmms2-plugin-id3v2 xmms2-plugin-mad xmms2-plugin-mp4 xmms2-plugin-vorbis
 
 ######################
 ### Bluetooth
@@ -52,5 +54,9 @@ echo "need to set up con files for sites..."
 P="/home/dtrckd/Desktop/workInProgress/conf/etc/nginx/sites-available/"
 cp $P/* /etc/nginx/sites-enabled/
 service nginx restart
+
+# push apt-source and etc file.. ?
+# Intall Mongo
+# Install Virtualbow
 
 #./install_from_scratch
