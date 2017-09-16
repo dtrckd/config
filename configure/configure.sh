567bb1ec7cf436a65d45bab86441639b14814570
dtrckd <ddtracked@gmail.com>
2017-09-16 20:35:32 +0200

    t

--- configure/configure.sh
+++ configure/configure.sh
@@ -9,12 +9,12 @@ mkdir -p ~/Music ~/Documents ~/Videos ~/SC ~/Desktop ~/src ~/bin
 ######################
 ### System
 ######################
-apt-get install sudo aptitude python3-pip make ctags apt-file apt-show-versions htop strace vim git 
+sudo apt-get install sudo aptitude python3-pip make ctags apt-file apt-show-versions htop strace vim git 
 
 ######################
 ### Utils
 ######################
-aptitude install elinks mc rsync byobu wireshark ranger bmon wicd nmap wget curl
+sudo aptitude install elinks mc rsync byobu wireshark ranger bmon wicd nmap wget curl
 ranger --copy-config=all
 
 ######################
@@ -26,7 +26,7 @@ pip3 install --user ipython jupyter jupyter pyside matplotlib numpy scipy
 ######################
 ### Apps
 ######################
-aptitude install firefox thunderbird midori gimp libreoffice
+sudo aptitude install thunderbird gimp libreoffice elinks w3m# firefox midori
 
 ######################
 ### Music (xmms2 plugin)
