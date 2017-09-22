b3f223d541c005025845953b633b480da44d1670
dtrckd <ddtracked@gmail.com>
2017-09-22 12:33:08 +0200

    d

--- diffdot.sh
+++ diffdot.sh
@@ -6,16 +6,19 @@ Help='-v verbose \n
 * help...'
 
 if [ "$Verbose" == "-v" ]; then
+    # Show all files that differs (all depth)
     for _f in $(find dotfiles/ -type f); do
         diff -q $_f ${HOME}${_f#dotfiles}
     done
 elif [ "$Verbose" == "-e" ]; then
+    # Edit files that differs (first depth)
     files=$(find dotfiles/ -maxdepth 1 -type f -exec \
         sh -c 'diff -q {} ${HOME}/$(basename {})' \; | sed 's/ /\n/g' |  grep  '\.')
     if [ ! -z "$files" ]; then
         vimdiff $files
     fi
 elif [ -z $Verbose ]; then
+    # Show files that differs (first depth)
     find dotfiles/ -maxdepth 1 -type f -exec \
         sh -c 'diff -q {} ${HOME}/$(basename {})' \;
 else
