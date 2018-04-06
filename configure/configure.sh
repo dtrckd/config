e9b15a0b5df5fbdd5db90a0543750f4426c34e57
dtrckd <ddtracked@gmail.com>
2018-04-06 14:42:45 +0200

    color

--- configure/configure.sh
+++ configure/configure.sh
@@ -2,6 +2,7 @@
 
 AGGRESSIVE=0
 PERS=0
+OPTS="-y"
 
 #
 # Init
@@ -10,48 +11,50 @@ rmdir --ignore-fail-on-non-empty   ~/Public/ Templates/
 mkdir -p ~/Music ~/Documents ~/Videos ~/SC ~/Desktop ~/src ~/bin
 cp blue/* ~
 
+# apt install sudo aptitude vim vim.nox firmaware-linux-nonfree
+
 ######################
 ### System
 ######################
-sudo aptitude install -R sudo aptitude psmisc python3-pip python3-setuptools rfkill apt-file apt-show-versions htop strace 
-pip3 install --upgrade setuptools wheel
+sudo aptitude install $OPTS -R sudo aptitude psmisc python3-pip python3-setuptools rfkill apt-file apt-show-versions htop strace 
+pip3 install $OPTS --upgrade setuptools wheel
 
 ######################
 ### Utils
 ######################
-sudo aptitude install -R mc rsync byobu ranger wicd vim-nox git gitk gitg
+sudo aptitude install $OPTS -R mc rsync byobu ranger wicd vim-nox git gitk gitg
 ranger --copy-config=all
 # Optionals (but advised !)
 if [ $AGGRESSIVE == 1 ]; then 
-    aptitude install -R apt-listbugs zip xclip acpi bmon nmap curl wget wireshark elinks w3m ksysguard
+    aptitude install $OPTS -R apt-listbugs zip xclip acpi bmon nmap curl wget wireshark elinks w3m ksysguard iotop
 fi
 
 ######################
 ### Python Dev
 ######################
-sudo aptitude install -R gfortran libopenblas-dev python3-tk
+sudo aptitude install $OPTS -R gfortran libopenblas-dev python3-tk
 if [ $AGGRESSIVE == 1 ]; then 
-    sudo aptitude install -R build-essential autoconf libtool pkg-config  python3-dev cython
+    sudo aptitude install  $OPTS-R build-essential autoconf automake libtool pkg-config  python3-dev cython
 fi
-pip3 install --user ipython jupyter matplotlib numpy scipy
+pip3 install $OPTS --user ipython jupyter matplotlib numpy scipy
 
 ######################
 ### Apps
 ######################
 if [ $AGGRESSIVE == 1 ]; then 
-    sudo aptitude install pandoc parallel
-    pip3 install --user pypandoc markdown2ctags
+    sudo aptitude install $OPTS pandoc parallel
+    pip3 install $OPTS --user pypandoc markdown2ctags
 fi
 
 if [ $PERS == 1 ]; then 
-    sudo aptitude install -R thunderbird gimp libreoffice # firefox midori
+    sudo aptitude install $OPTS -R thunderbird gimp libreoffice # firefox midori
 fi
 
 ######################
 ### Music (xmms2 plugin)
 ######################
 if [ $PERS == 1 ]; then 
-    sudo aptitude -R install vlc audacity xmms2 gxmms2 \
+    sudo aptitude  install $OPTS -R vlc audacity xmms2 gxmms2 \
         xmms2-plugin-alsa xmms2-plugin-asf xmms2-plugin-avcodec xmms2-plugin-faad xmms2-plugin-flac xmms2-plugin-id3v2 xmms2-plugin-mad xmms2-plugin-mp4 xmms2-plugin-vorbis
     # (mp3cut) => poc-streamer
 fi
@@ -61,7 +64,7 @@ fi
 ### Latex
 ######################
 if [ $PERS == 1 ]; then 
-    sudo aptitude install -R texlive texlive-latex-extra texlive-science texlive-fonts-extra texlive-latex-base texlive-extra-utils texlive-bibtex-extra texlive-plain-generic
+    sudo aptitude install $OPTS -R texlive texlive-latex-extra texlive-science texlive-fonts-extra texlive-latex-base texlive-extra-utils texlive-bibtex-extra texlive-plain-generic
 fi
 
 
@@ -69,7 +72,7 @@ fi
 ### Bluetooth
 ######################
 if [ $PERS == 1 ]; then 
-    sudo aptitude -R install pulseaudio-module-bluetooth blueman
+    sudo aptitude install $OPTS -R  pulseaudio-module-bluetooth blueman
     pactl load-module module-bluetooth-discover
 fi
 
@@ -81,8 +84,8 @@ if [ $PERS == 1 ]; then
     # @debug make webmain links...
     echo "Installing nginx..."
     # Kill apache ?
-    sudo aptitude install nginx 
-    sudo aptitude install php-fpm php php-cgi uwsgi
+    sudo aptitude install $OPTS nginx 
+    sudo aptitude install $OPTS php-fpm php php-cgi uwsgi
     echo "need to set up con files for sites..."
     P="/home/dtrckd/Desktop/workInProgress/conf/etc/nginx/sites-available/"
     sudo cp $P/* /etc/nginx/sites-enabled/
@@ -90,7 +93,7 @@ if [ $PERS == 1 ]; then
 fi
 
 # Packaging
-./setup-packaging.sh
+#./setup-packaging.sh
 
 # Various
 #./install_from_scratch
