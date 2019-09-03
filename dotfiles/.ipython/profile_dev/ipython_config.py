ff5482c230119ec06f386e29965e213972811bc9
dtrckd <dtrckd@gmail.com>
2019-09-03 14:44:40 -0100

    sklearn a color

--- dotfiles/.ipython/profile_dev/ipython_config.py
+++ dotfiles/.ipython/profile_dev/ipython_config.py
@@ -4,7 +4,15 @@ c.InteractiveShellApp.exec_lines = [
     'import sys, os, re, json, pickle',
     'import numpy as np',
     'import scipy as sp',
+    'import scipy.sparse as sparse',
     'from scipy.sparse import csr_matrix, csc_matrix, coo_matrix',
     'import pandas as pd',
     'import matplotlib.pyplot as plt',
+    'import sklearn',
+    'from sklearn.preprocessing import StandardScaler, Normalizer, Binarizer, LabelEncoder, OneHotEncoder, OrdinalEncoder, LabelBinarizer, PolynomialFeatures',
+    'from sklearn import metrics',
+]
+
+c.AliasManager.user_aliases = [
+ ('la', 'ls -al')
 ]
