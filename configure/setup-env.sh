2c285bd2becb1bccc4c90ffe194b8ca769aa537c
dtrckd <dtrckd@gmail.com>
2021-01-18 19:09:26 +0100

    fix go update

--- configure/setup-env.sh
+++ configure/setup-env.sh
@@ -10,22 +10,20 @@ fi
 
 if [ "$Target" == "go" ]; then
     # Golang
-
-    # First approach
-    #GOTARGET="go1.11.8.linux-amd64.tar.gz "
-    #wget https://storage.googleapis.com/golang/$GOTARGET
-    #sudo tar -zxvf  $GOTARGET -C /usr/local/
-    #rm $GOTARGET
-    #mkdir -p $HOME/.go
-
-    # Go update approach
-    release=$(curl --silent https://golang.org/doc/devel/release.html | grep -Eo 'go[0-9]+(\.[0-9]+)+' | sort -V | uniq | tail -1)
     os=$(uname -s | tr '[:upper:]' '[:lower:]')
     arch=$(case "$(uname -m)" in i*) echo '386' ;; x*) echo 'amd64' ;; *) echo 'armv61'; esac)
 
+    # Go update approach
+    release=$(curl --silent https://golang.org/doc/devel/release.html | grep -Eo 'go[0-9]+(\.[0-9]+)+' | sort -V | uniq | tail -1)
+    echo "lastet found release: $release"
+    release="go1.15.6"
+
     VER="$release.$os-$arch"
     sudo mkdir -p "/usr/local/lib/$VER"
-    curl --silent "https://storage.googleapis.com/golang/$VER.tar.gz" | sudo tar -vxz --strip-components 1 -C "/usr/local/lib/$VER"
+    #URL="https://storage.googleapis.com/golang/$VER.tar.gz"
+    URL="https://dl.google.com/go/$VER.tar.gz"
+    echo "downloading: $URL"
+    curl $URL | sudo tar -vxz --strip-components 1 -C "/usr/local/lib/$VER"
     sudo rm -f /usr/local/bin/go
     sudo rm -f /usr/local/bin/gofmt
     sudo ln -s "/usr/local/lib/$VER/bin/go" /usr/local/bin/go
