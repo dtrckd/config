2e3be4ffac9fff7d0367197e33da95a9aecd0050
dtrckd <dtrckd@gmail.com>
2021-06-15 11:05:26 +0200

    fix server

--- configure/configure_server.sh
+++ configure/configure_server.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+CREATE_USER=0
+INSTALL_NGINX=0
+
 # cat configure_server.sh | ssh user@remote_adrr
 # wget https://raw.githubusercontent.com/dtrckd/config/master/configure/configure_server.sh
 
-USERNAME="admin"
 ##################################
 ###### Install Base Package ######
 ##################################
@@ -12,7 +14,6 @@ apt-get upgrade -y
 
 apt-get install -y htop git make psmisc vim mc rsync tmux ranger curl wget # basic
 apt-get install -y net-tools nmap # network
-apt-get install -y vim  mc rsync tmux ranger curl wget # edition
 apt-get install -y python3-setuptools python-pip python3-pip # python
 apt-get autoclean -y
 apt-get autoremove -y
@@ -21,17 +22,15 @@ apt-get clean -y
 #pip3 install -U pip setuptools wheel
 #pip3 install -U cython
 
-##################################
-###### Install Server | nginx/certbot/security ######
-##################################
-sudo apt-get install -y nginx # server
-sudo apt-get install -y fail2ban # secu
 # Certbot/HTTPS
 #sudo apt-get install software-properties-common
 #sudo add-apt-repository ppa:certbot/certbot
 #sudo apt-get update
 sudo apt-get install -y software-properties-common
 sudo apt-get install -y certbot python3-certbot-nginx
+
+# Security
+sudo apt-get install -y fail2ban # secu
 
 ##################################
 ###### fix Hostnames        ######
@@ -50,37 +49,49 @@ apt-get clean -y
 systemctl reload nginx
 END
 
-#
-# |adduser user| from script ??
-#
-
-##################################
-###### Add New User Programmatically
-##################################
-useradd --create-home -d /home/$USERNAME --shell /bin/bash  $USERNAME -p $(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w ${1:-64} | head -n 1)
-usermod -aG sudo $USERNAME
-
-HOMEUSER=$(getent passwd $USERNAME | cut -d: -f6) # === /home/USER/
-
-cp -r ~/.ssh $HOMEUSER/
-chown -R $USERNAME:$USERNAME $HOMEUSER/.ssh/
-
-cat >> $HOMEUSER/.bashrc << EOF
-if [ -f ~/.bash_profile ]; then
-    . ~/.bash_profile
+if [ "$INSTALL_USER" == 1 ]; then
+    ##################################
+    ###### Install Server | nginx/certbot/security ######
+    ##################################
+    sudo apt-get install -y nginx # server
 fi
-EOF
 
-#su - $USERNAME
-cd /home/$USERNAME
+if [ "$CREATE_USER" == 1 ]; then
 
-# Authorize github
-sudo -H -u $USERNAME bash -c 'ssh-keyscan github.com >> ~/.ssh/known_hosts'
-#sudo -H -u $USERNAME bash -c 'ssh-keygen -lf githubKey'
+    USERNAME="admin"
 
-##################################
-###### Setup New User Programmatically
-##################################
-sudo -H -u $USERNAME bash -c 'git clone https://github.com/dtrckd/config.git src/config'
-sudo -H -u $USERNAME bash -c 'cd src/config/ && make configure_server'
+    #
+    # |adduser user| from script ??
+    #
 
+    ##################################
+    ###### Add New User Programmatically
+    ##################################
+    useradd --create-home -d /home/$USERNAME --shell /bin/bash  $USERNAME -p $(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w ${1:-64} | head -n 1)
+    usermod -aG sudo $USERNAME
+
+    HOMEUSER=$(getent passwd $USERNAME | cut -d: -f6) # === /home/USER/
+
+    cp -r ~/.ssh $HOMEUSER/
+    chown -R $USERNAME:$USERNAME $HOMEUSER/.ssh/
+
+    cat >> $HOMEUSER/.bashrc << EOF
+    if [ -f ~/.bash_profile ]; then
+        . ~/.bash_profile
+    fi
+    EOF
+
+    #su - $USERNAME
+    cd /home/$USERNAME
+
+    # Authorize github
+    sudo -H -u $USERNAME bash -c 'ssh-keyscan github.com >> ~/.ssh/known_hosts'
+    #sudo -H -u $USERNAME bash -c 'ssh-keygen -lf githubKey'
+
+    ##################################
+    ###### Setup New User Programmatically
+    ##################################
+    sudo -H -u $USERNAME bash -c 'git clone https://github.com/dtrckd/config.git src/config'
+    sudo -H -u $USERNAME bash -c 'cd src/config/ && make configure_server'
+
+fi
