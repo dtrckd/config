f2a2317405437dc9b7b540c82cba4df150210339
dtrckd <dtrckd@gmail.com>
2022-06-28 01:48:24 +0200

    swap.sh

---
+++ snippets/swap.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/bash
+
+# Create a swap on on VPS
+sudo fallocate -l 4G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+
+# Optimize
+sudo sysctl vm.swappiness=10
+sudo sysctl vm.vfs_cache_pressure=50
+echo 'vm.swappiness=10' | sudo tee -a /etc/sysctl.conf
+echo 'vm.vfs_cache_pressure=50' | sudo tee -a /etc/sysctl.conf
+
+# Setup at bootime
+sudo cp /etc/fstab /etc/fstab.bak
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+
+# Check
+sudo swapon --show
+free -h
+
